2e32af1d0520ef864e088192a1f869911bf96f41
eduardojigub <eduardobrunoit@gmail.com>
2022-08-02 16:56:53 -0300

    creating function timeToSeconds

--- alura-studies/src/components/Cronometer/index.tsx
+++ alura-studies/src/components/Cronometer/index.tsx
@@ -1,8 +1,10 @@
+import { timeToSeconds } from "../../common/utils/date";
 import Button from "../Button";
 import Clock from "./Clock";
 import "./style.scss";
 
 const Cronometer = () => {
+  console.log("Conversão: " + timeToSeconds("01:01:01"));
   return (
     <div className="cronometer">
       <p className="title">Pick a card and start the counter!</p>
