e1586d52f028ea6fbeea69bfdbeae7539dd54a73
eduardojigub <eduardobrunoit@gmail.com>
2022-08-01 23:37:58 -0300

    feat: controlling forms

--- alura-studies/src/components/Forms/index.tsx
+++ alura-studies/src/components/Forms/index.tsx
@@ -3,16 +3,33 @@ import Button from "../Button";
 import "./style.scss";
 
 class Form extends React.Component {
+  state = {
+    task: "",
+    time: "00:00",
+  };
+
+  addTask = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("state: ", this.state);
+  };
+
   render(): React.ReactNode {
     return (
       <>
-        <form className="newTask">
+        <form className="newTask" onSubmit={this.addTask}>
           <div className="inputContainer">
             <label htmlFor="task">Add a new task</label>
             <input
               type="text"
               name="task"
               id="task"
+              value={this.state.task}
+              onChange={(event) =>
+                this.setState({
+                  ...this.state,
+                  task: event.target.value,
+                })
+              }
               placeholder="What do you want to do?"
               required
             />
@@ -23,6 +40,13 @@ class Form extends React.Component {
               type="time"
               step="1"
               name="time"
+              value={this.state.time}
+              onChange={(event) =>
+                this.setState({
+                  ...this.state,
+                  time: event.target.value,
+                })
+              }
               id="time"
               min="00:00:00"
               max="01:30:00"
