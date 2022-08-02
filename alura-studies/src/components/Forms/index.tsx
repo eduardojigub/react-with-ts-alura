9997ec454b9895b42c6b900351a326aab8169402
eduardojigub <eduardobrunoit@gmail.com>
2022-08-01 21:44:04 -0300

    feat: addin form label

---
+++ alura-studies/src/components/Forms/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Button from "../Button";
+
+class Form extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <>
+        <form>
+          <div>
+            <label htmlFor='task'>
+                Add a new task
+            </label>
+            <input 
+            type="text"
+            name="task"
+            id="task"
+            placeholder="What do you want to do?"
+            required
+            />
+          </div>
+          <div>
+            <label htmlFor="time">
+                Time
+            </label>
+            <input 
+            type="time"
+            step="1"
+            name="time"
+            id="time"
+            min="00:00:00"
+            max="01:30:00"
+            required
+            />
+          </div>
+          <Button />
+        </form>
+      </>
+    );
+  }
+}
+
+export default Form;
