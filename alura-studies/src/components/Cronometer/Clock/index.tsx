ed88742699ab3a9ffbaa9a307c36014479d4cdfb
eduardojigub <eduardobrunoit@gmail.com>
2022-08-02 17:31:14 -0300

    feat adding time to cronometer

--- alura-studies/src/components/Cronometer/Clock/index.tsx
+++ alura-studies/src/components/Cronometer/Clock/index.tsx
@@ -4,14 +4,19 @@ interface Props {
   time: number | undefined;
 }
 
-const Clock = ({ time }: Props) => {
+const Clock = ({ time = 0 }: Props) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  const [minutoDezena, minutoUnidade] = String(minutes).padStart(2, "0");
+  const [segundoDezena, segundoUnidade] = String(seconds).padStart(2, "0");
+
   return (
     <>
-      <span className="clockNumber ">0</span>
-      <span className="clockNumber ">0</span>
+      <span className="clockNumber ">{minutoDezena}</span>
+      <span className="clockNumber ">{minutoUnidade}</span>
       <span className="clockDivision">:</span>
-      <span className="clockNumber ">0</span>
-      <span className="clockNumber ">0</span>
+      <span className="clockNumber ">{segundoDezena}</span>
+      <span className="clockNumber ">{segundoUnidade}</span>
     </>
   );
 };
